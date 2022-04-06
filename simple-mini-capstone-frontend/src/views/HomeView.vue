3977b4d97d7470deb7864b7090f63dc8c55cd120
Will Grund <willgrund178@gmail.com>
2022-04-06 13:46:57 -0400

    adds recipes

---
+++ simple-mini-capstone-frontend/src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  data: function() {
+    return {
+      message: "Welcome to The Cookbook!"
+      recipes: [
+        {id: 1, title: "Eggs", chef: "Chicken"}
+        {id: 2, title: "Watermelon", chef: "Watermelon Plant"}
+      ],
+    };
+  },
+  created: function() {},
+  methods: {}
+};
+</script>
+
+<template>
+  <div class="home">
+    <h1>{{ message }}</h1>
+    <h2 v-for="recipe in recipes">{{ recipe }}</h2>
+  </div>
+</template>
+
+<style></style>
